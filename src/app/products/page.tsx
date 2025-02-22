03968116d7ae28f7747418cc7dd77b1e41ca6e6e
Benoni brandao <benonibrandao2004@gmail.com>
2025-02-22 17:40:45 -0300

    feat:add instal setup componets

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const productsPage = () => {
+
+  return <div className=" p-5 border border-red-500 rounded-x1">
+    <Input placeholder="vamos fechar negocio"></Input>
+    <Button>vinho 0.7</Button>
+    </div>
+};
+
+export default productsPage
