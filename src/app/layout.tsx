618a93f2698fa7ae5d06b3e6c0916b769ca8d523
Benoni brandao <benonibrandao2004@gmail.com>
2025-02-22 18:57:02 -0300

    chore uptade fot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
+// Toda seção global da minha aplicação
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dmSans = DM_Sans({
+  weight:["100", "200", "300", "400","500","900", "800"], 
   subsets: ["latin"],
 });
 
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dmSans.className} antialiased`}
       >
         {children}
       </body>
